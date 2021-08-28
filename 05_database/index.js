c78390168497b674685aa24e56ba1125dd6ee410
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-08-28 21:21:06 +0200

    add 05_database
    add icons for database and forms
    
    add title and description for forms
    
    change credentials

--- 05_database/index.js
+++ 05_database/index.js
@@ -1,6 +1,7 @@
 // Importieren von Code-Bibliotheken
 var express = require('express');
 var mustacheExpress = require('mustache-express');
+var Pool = require('pg').Pool                         /* start=4 */
 
 var app = express();
 var port = 3000;
@@ -10,14 +11,24 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+var pool = new Pool({                               /* start=5 */
+  user: 'MEINBENUTZERNAME',                         /* start=5 */
+  host: 'localhost',                                /* start=5 */
+  database: 'MEINEDATENBANK',                       /* start=5 */
+  password: 'MEINPASSWORT',                         /* start=5 */
+  port: 5432,                                       /* start=5 */
+})                                                  /* start=5 */
+
 // Routen
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/uebermich', (req, res) => {
-  res.render('about');
-});
+app.get('/todos', (req, res) => {                                           /* start=6 */
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, result) => {    /* start=6 */
+    res.render('todos', { todos: result.rows });                            /* start=7 */
+  });                                                                       /* start=6 */
+});                                                                         /* start=6 */
 
 // Serverstart
 app.listen(port, () => {
---
+++ 05_database/konsole.sh
@@ -0,0 +1,2 @@
+cd pfad/zu/meinem/projekt         # start=2
+npm install pg                    # start=3
