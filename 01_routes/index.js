0a283e22f8cf093d656ff988469fe3f17b420ecd
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-08-28 18:25:39 +0200

    add 01_routes

---
+++ 01_routes/index.js
@@ -0,0 +1,22 @@
+// Importieren von Code-Bibliotheken
+var express = require('express');
+
+var app = express();
+var port = 3000;
+
+// Konfiguration und Einstellungen
+
+// Routen
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/contact', (req, res) => {                                     /* start=1 */
+  res.send('BBZ Biel, Wasenstrasse 5, 2502 Biel');                      /* start=1 */
+});                                                                     /* start=1 */
+
+
+// Serverstart
+app.listen(port, () => {
+  console.log('App listening on port ' + port);
+});
