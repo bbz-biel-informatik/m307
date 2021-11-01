f05741be46f33895e45825794d962d8215d7cfa2
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-11-01 10:51:00 +0100

    add 13_registrierung

---
+++ 13_registrierung/index.js
@@ -0,0 +1,37 @@
+// Importieren von Code-Bibliotheken
+var express = require('express');
+var mustacheExpress = require('mustache-express');
+var cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');                 /* start=4 */
+
+var Pool = require('pg').Pool;
+
+var app = express();
+var port = 3000;
+
+// Konfiguration und Einstellungen
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+// Routen
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/registration_form', function(req, res) {           /* start=5 */
+  res.render('registration_form');                           /* start=5 */
+});                                                   /* start=5 */
+
+app.post('/register', function (req, res) {                         /* start=6 */
+  var passwort = bcrypt.hashSync(req.body.passwort, 10);            /* start=6 */
+  pool.query(`INSERT INTO users (username, passwort) VALUES ('${req.body.benutzername}', '${passwort}')`, (error, result) => { /* start=7 */
+    if(error) { throw error; }                                      /* start=7 */
+    res.redirect('/login');                                         /* start=7 */
+  });                                                               /* start=7 */
+});                                                                 /* start=6 */
+
+// Serverstart
+app.listen(port, () => {
+  console.log('App listening on port ' + port);
+});
