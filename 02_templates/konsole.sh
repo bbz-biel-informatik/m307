3110f02a83c3cbaf763fe895c15e914819ef8e3b
Lukas_Skywalker <lukas.diener@hotmail.com>
2022-11-08 10:17:26 +0100

    Remove old tutorials

--- 02_templates/konsole.sh
+++
@@ -1,2 +0,0 @@
-cd pfad/zu/meinem/projekt         # start=2
-npm install mustache-express      # start=3
--- 05_database/konsole.sh
+++
@@ -1,2 +0,0 @@
-cd pfad/zu/meinem/projekt         # start=2
-npm install pg                    # start=3
